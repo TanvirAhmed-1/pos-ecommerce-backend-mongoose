d4978aeef3c156518ccb1e3412caf37d69da20b0
Tanvir Ahmed <tanvirahmed1078@gmail.com>
2026-03-29 20:41:27 +0600

    fix code

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -1,8 +1,7 @@
 import { Router } from "express";
 import { PaymentControllers } from "./payment.controller";
-import auth from "../../middlewares/auth";
 
 const router = Router();
-router.get("/bkash-callback", auth("user"), PaymentControllers.bkashCallback);
+router.get("/bkash-callback", PaymentControllers.bkashCallback);
 
 export const PaymentRoutes = router;
